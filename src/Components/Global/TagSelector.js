b18ddf4bd42c041766844bd6c510573cb774005f
razaqisama <107979029+razaqi@users.noreply.github.com>
2022-09-08 01:48:11 +0700

    beautify tag selector

--- src/Components/Global/TagSelector.js
+++ src/Components/Global/TagSelector.js
@@ -29,9 +29,16 @@ export const TagSelector = () => {
 
     return (
         <div className="sticky top-0 flex flex-row items-center border-b-[1px] bg-gray-900 my-2">
-            <svg onClick={handlePrev} className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-            </svg>
+            <div className="relative w-6 h-6 flex justify-center items-center group">
+                <svg onClick={handlePrev} 
+                    style={{
+                        background: "rgba(0, 0, 0, 0) linear-gradient(270deg, rgba(17, 24, 39, 0) 0px, rgb(17, 24, 39) 50%) repeat scroll 0% 0%"
+                    }}
+                    className="absolute z-[99] cursor-pointer w-14 h-6 transform group-hover:-translate-x-1 transition duration-300 ease-in-out" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                </svg>
+            </div>
+
             <div 
                 className="flex-auto overflow-x-auto"
                 ref={scrollRef}
@@ -49,9 +56,15 @@ export const TagSelector = () => {
                     }
                 </div>
             </div>
-            <svg onClick={handleNext} className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-            </svg>
+            <div className="relative w-6 h-6 flex justify-center items-center group">
+                <svg 
+                    style={{
+                        background: "rgba(0, 0, 0, 0) linear-gradient(90deg, rgba(17, 24, 39, 0) 0px, rgb(17, 24, 39) 50%) repeat scroll 0% 0%"
+                    }}
+                    onClick={handleNext} className="absolute right-0 bg-b z-[99] cursor-pointer w-14 h-6 transform translate-x-[24px] group-hover:translate-x-[30px] transition duration-300 ease-in-out" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                </svg>
+            </div>
         </div>
     )
 }
